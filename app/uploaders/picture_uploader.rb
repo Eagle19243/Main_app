82cbc6d7eff71cfd62d31d4179e6e1dca4e0c3c4
Moussa Sarr <moussasarr49@gmail.com>
2016-04-26 17:49:02 -0400

    Solved image and task issues

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -3,8 +3,8 @@
 class PictureUploader < CarrierWave::Uploader::Base
 
    # Include RMagick or MiniMagick support:
-   #include CarrierWave::RMagick
-   include CarrierWave::MiniMagick
+   #   include CarrierWave::RMagick
+     include CarrierWave::MiniMagick
   process resize_to_fit: [400, 400]
 
   if Rails.env.production?
