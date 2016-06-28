8ed2715d2d2f5289d5b12882dc144af3dcb03b1a
anton <ant.punko@gmail.com>
2016-06-28 19:36:26 +0300

    Add 4 additional admins

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -5,6 +5,24 @@ class CreateAdminService
         user.password_confirmation = Rails.application.secrets.admin_password
         user.name = Rails.application.secrets.admin_name
         user.admin!
-      end
+    end
   end
+
+  def create_additional_admins
+    count = 0
+    admins = Rails.application.secrets.additional_admins
+    if !admins.nil?
+      admins.each do |value|
+        user = User.find_or_create_by!(email: value["email"]) do |admin|
+          admin.password = value["password"]
+          admin.password_confirmation = value["password"]
+          admin.name = value["name"]
+          admin.admin!
+        end
+        count += 1
+      end
+    end
+    count
+  end
+
 end
