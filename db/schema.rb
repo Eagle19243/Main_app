f9f04e425d7e963a0fc21371443613822f3bf27c
tomahawk <tmutunhire@gmail.com>
2016-07-07 19:47:40 +0200

    migrated the database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160707112642) do
+ActiveRecord::Schema.define(version: 20160707173252) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id"
@@ -109,6 +109,14 @@ ActiveRecord::Schema.define(version: 20160707112642) do
 
   add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id"
   add_index "messages", ["user_id"], name: "index_messages_on_user_id"
+
+  create_table "notifications", force: :cascade do |t|
+    t.string   "type"
+    t.string   "summary"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "proj_admins", force: :cascade do |t|
     t.integer  "user_id"
