5514c63bbad3430c89cd6c958878285be0586978
tomahawk <tmutunhire@gmail.com>
2016-07-07 12:26:54 +0200

    run database migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706105653) do
+ActiveRecord::Schema.define(version: 20160707101004) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id"
@@ -178,6 +178,15 @@ ActiveRecord::Schema.define(version: 20160706105653) do
     t.boolean  "assigned",                      default: false
     t.text     "proof_of_execution"
     t.text     "short_description"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "number_of_members"
+    t.integer  "number_of_projects"
+    t.text     "mission"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
   create_table "users", force: :cascade do |t|
