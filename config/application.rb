f1ff4de5ada4f93e6460079be64eeee7e7d10d56
tomahawk <tmutunhire@gmail.com>
2016-07-09 00:04:06 +0200

    problem with static files, see public static

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module YouServe
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    
+
 
     # For Foundation 5
     config.assets.precompile += %w( vendor/modernizr )
@@ -27,5 +27,6 @@ module YouServe
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_files = true
   end
 end
