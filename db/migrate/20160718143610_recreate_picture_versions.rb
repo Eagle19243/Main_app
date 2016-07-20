cbeeb9dd2b96962b1ed608f015307cbc6a5da02f
tomahawk <tmutunhire@gmail.com>
2016-07-20 12:06:57 +0200

    reduce error prone

--- db/migrate/20160718143610_recreate_picture_versions.rb
+++ db/migrate/20160718143610_recreate_picture_versions.rb
@@ -1,7 +1,7 @@
 class RecreatePictureVersions < ActiveRecord::Migration
   def change
     Project.find_each do |user|
-      user.picture.recreate_versions!
+      user.picture.try(:recreate_versions)
     end
   end
 end
