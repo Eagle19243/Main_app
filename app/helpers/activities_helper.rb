594928862395d662ebbda5c8f55a6ad3b816a12c
tomahawk <tmutunhire@gmail.com>
2016-07-26 02:47:17 +0200

    format the notification links and text

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -11,7 +11,32 @@ module ActivitiesHelper
     object = activity.targetable
     content_tag(:div, nil, class: 'pro-text') do
       [
-        (link_to object.title, object),
+        # (link_to object.title, object),
+        (link_to activity.try(:targetable_type), link_path(activity, object) ),
+        " was successful #{activity.action}"
+      ].join.html_safe
+    end
+  end
+
+  def get_id(object)
+    object_id = 1
+    object_id = object.try(:id).nil? ? 1 : object.id
+    object_id
+  end
+
+  def link_path(activity, object)
+    #  get_link = "#{activity.targetable_type.underscore}".concat("_path(object)")
+     get_link = "#{activity.targetable_type.underscore.pluralize}"
+    #  send(get_link, object.try(:id), id: 2, Rails.application.routes.url_helpers)
+    Rails.application.routes.url_for controller: get_link, action: :show, id: get_id(object), only_path: true
+  end
+
+  def user_notifier(activity)
+    object = activity.targetable
+    content_tag(:div, nil, class: 'pro-text') do
+      [
+        # (link_to object.title, object),
+        (link_to activity.try(:targetable_type), object),
         " was successful #{activity.action}"
       ].join.html_safe
     end
