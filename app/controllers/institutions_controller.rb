7f5bae357be9f3e1f9c2d8f996317c63657ff2ec
tomahawk <tmutunhire@gmail.com>
2016-07-29 22:02:54 +0200

    institution url

--- app/controllers/institutions_controller.rb
+++ app/controllers/institutions_controller.rb
@@ -69,6 +69,6 @@ class InstitutionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def institution_params
-      params.require(:institution).permit(:name, :description, :country, :city, :logo)
+      params.require(:institution).permit(:name, :description, :country, :city, :logo, :url)
     end
 end
