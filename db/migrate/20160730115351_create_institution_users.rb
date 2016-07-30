eb8bdf37e89d82ff64d2157c2f9f740eecd22ac3
tomahawk <tmutunhire@gmail.com>
2016-07-30 14:08:30 +0200

    create InstitutionUser

---
+++ db/migrate/20160730115351_create_institution_users.rb
@@ -0,0 +1,9 @@
+class CreateInstitutionUsers < ActiveRecord::Migration
+  def change
+    create_table :institution_users do |t|
+      t.references :institution, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+      t.string :position
+    end
+  end
+end
