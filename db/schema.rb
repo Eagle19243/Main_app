97593357c97c74e456ec0768a93bbb1aa6b9b7d2
tomahawk <tmutunhire@gmail.com>
2016-07-31 00:04:57 +0200

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160730121517) do
+ActiveRecord::Schema.define(version: 20160730215216) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id"
@@ -255,6 +255,9 @@ ActiveRecord::Schema.define(version: 20160730121517) do
     t.string   "phone_number",           limit: 8
     t.integer  "institution_id"
     t.text     "bio"
+    t.string   "facebook_url"
+    t.string   "twitter_url"
+    t.string   "linkedin_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
