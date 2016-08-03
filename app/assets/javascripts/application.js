3ddbe12d7a774c38284e63e0742ef237da3f3885
tomahawk <tmutunhire@gmail.com>
2016-08-03 20:50:09 +0200

    turbolinks a problem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,3 +34,24 @@ $(document).off('page:load').on('page:load', function() {
     console.log( "ready!" );
     $(document).foundation();
 });
+
+
+
+// enhance Turbolinks when necessary
+// https://coderwall.com/p/ii0a_g/page-reload-refresh-every-5-sec-using-turbolinks-js-rails-jquery
+// this code may be removed without harmful side effects
+$(document).on('ready page:load', function() {
+    var REFRESH_INTERVAL_IN_MILLIS = 5000;
+     if ($('.f-pending-message').length > 0) {
+       setTimeout(function(){
+        #disable page scrolling to top after loading page content
+        Turbolinks.enableTransitionCache(true);
+
+        # pass current page url to visit method
+        Turbolinks.visit(location.toString());
+
+        #enable page scroll reset in case user clicks other link
+        Turbolinks.enableTransitionCache(false);
+         }, REFRESH_INTERVAL_IN_MILLIS);
+    }
+});
