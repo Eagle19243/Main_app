fe747e4cde78514c7ab3bebaa1cd9ac3a3a5fdec
anton <ant.punko@gmail.com>
2016-08-06 19:12:02 +0300

    added paypal code from "stage_paypal_ipnfix" branch

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -3,15 +3,5 @@ class Donation < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :task
 
-
- validates_numericality_of :amount, :only_integer => true, :greater_than_or_equal_to => 1
-
-
-
-
-
-
-
-
-
+  validates_numericality_of :amount, :only_integer => true, :greater_than_or_equal_to => 1
 end
