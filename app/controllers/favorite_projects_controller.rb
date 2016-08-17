7c810c74d482ba6e2c7106db434174d3ee3e57a8
anton <ant.punko@gmail.com>
2016-08-17 21:28:53 +0300

    Added favorite project model and controller

---
+++ app/controllers/favorite_projects_controller.rb
@@ -0,0 +1,7 @@
+class FavoriteProjectsController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+end
---
+++ app/models/favorite_project.rb
@@ -0,0 +1,4 @@
+class FavoriteProject < ActiveRecord::Base
+  belongs_to :user
+  validates_uniqueness_of :project_id, :scope => [:user_id]
+end
