a052a9536959d1e4609cb95b0f178b0f75397860
anton <ant.punko@gmail.com>
2016-08-21 11:26:52 +0300

    Added MessagePictureUploader

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,7 @@ class Message < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :body, :conversation_id, :user_id
 
-  mount_uploader :image, PictureUploader
+  mount_uploader :image, MessagePictureUploader
 
   def message_time
     created_at.strftime("%m/%d/%y at %l:%M %p")
--- app/uploaders/message_picture_uploader.rb
+++ app/uploaders/message_picture_uploader.rb
@@ -1,9 +1,8 @@
 # encoding: utf-8
 
-class PictureUploader < CarrierWave::Uploader::Base
+class MessagePictureUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
-  process resize_to_fit: [400, 400]
 
   if Rails.env.production?
     storage :fog
@@ -13,14 +12,6 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  version :thumb do
-    process resize_to_fit: [50, 50]
-  end
-
-  version :medium do
-    process resize_to_fill: [535, 350]
   end
 
   def extension_white_list
