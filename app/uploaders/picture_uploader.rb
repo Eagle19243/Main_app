b34efd50a6fe7896da488f3430bbdc3e1fb6bff8
anton <ant.punko@gmail.com>
2016-08-21 11:12:48 +0300

    Added attach file icon

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -3,7 +3,7 @@
 class PictureUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
-  process resize_to_fit: [400, 400]
+  #process resize_to_fit: [400, 400]
 
   if Rails.env.production?
     storage :fog
