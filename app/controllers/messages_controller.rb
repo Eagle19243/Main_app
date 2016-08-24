982e96409580b844886b3e667b2cba23fdb8ea82
AteqEjaz <ateq.odesk@gmail.com>
2016-08-24 16:32:36 +0500

    Refector firechat UI

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,15 @@ class MessagesController < ApplicationController
  layout false
 
  def index
-  @projects= current_user.projects.all
+  project_ids= Array.[]
+  all_Assignments =current_user.assignments.all
+  all_Assignments.each do |ass|
+   task = Task.find(ass.task_id)
+   project_ids << task.project_id
+  end
+  project_ids = project_ids.uniq
+  @projects= current_user.projects|Project.where(id: project_ids)
+   puts ''
  end
 
   def create_chat_room
