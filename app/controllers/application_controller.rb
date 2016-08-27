287edc539093c87d8b5af7c6bba0e00e40d4537e
Manish Kongari <manishkongari@outlook.com>
2016-08-27 12:12:30 +0530

    made assets to use to manish layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   #before_filter :admin_only_mode
-
+    layout "manish"
     #a filter to enable private mode in which the app is available only to admins
     def admin_only_mode
       unless current_user.try(:admin?)
