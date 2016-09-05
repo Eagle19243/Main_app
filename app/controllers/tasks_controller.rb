cbc4b850fecd29b0ca192a5b890a782c03f81e89
AteqEjaz <ateq.odesk@gmail.com>
2016-09-05 21:21:10 +0500

    Validate User and create wallets after acceptance of suggested task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :accept, :reject ]
+  before_action :validate_user, only:[:accept, :reject ]
   # GET /tasks
   # GET /tasks.json
 
@@ -26,18 +26,21 @@ class TasksController < ApplicationController
 
   # POST /tasks
   # POST /tasks.json
-   def create
+  def create
 
     @task = Task.new(task_params)
     @task.user_id = current_user.id
-   if @task.project.user_id != current_user.id
-     @task.state='suggested_task'
-   end
+    if @task.project.user_id != current_user.id
+      @task.state='suggested_task'
+    end
 
     respond_to do |format|
       if @task.save
+        unless @task.suggested_task?
+           @task.assign_address
+        end
         current_user.create_activity(@task, 'created')
-      
+
         format.html { redirect_to @task, notice: 'Task was successfully created.' }
         format.json { render :show, status: :created, location: @task }
       else
@@ -50,22 +53,22 @@ class TasksController < ApplicationController
   # PATCH/PUT /tasks/1
   # PATCH/PUT /tasks/1.json
   def update
-        respond_to do |format|
-   
-        @task.project_id = Task.find(params[:id]).project_id
+    respond_to do |format|
+
+      @task.project_id = Task.find(params[:id]).project_id
       if @task.update(task_params)
         activity = current_user.create_activity(@task, 'edited')
-        activity.user_id = current_user.id 
+        activity.user_id = current_user.id
         format.html { redirect_to @task, notice: 'Task was successfully updated.' }
         format.json { render :show, status: :ok, location: @task }
-      
+
       else
-        
+
         format.html  { render :edit }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
-   
+
   end
 
   # DELETE /tasks/1
@@ -81,11 +84,15 @@ class TasksController < ApplicationController
   end
 
   def accept
-    @task = Task.find(params[:id])
-    if @task.accept!
-      flash[:success] = "Task accepted"
-    else flash[:error] = "Task was not accepted"
+    previous = @task.suggested_task?
+    if  @task.accept!
+      flash[:success] = "Task accepted "
 
+      if previous
+        @task.assign_address
+      end
+    else
+      flash[:error] = "Task was not accepted"
     end
     redirect_to   taskstab_project_path(@task.project_id)
 
@@ -93,24 +100,31 @@ class TasksController < ApplicationController
   end
 
   def reject
-    @task = Task.find(params[:id])
-    if @task.reject!
+
+    if  @task.reject!
       flash[:success] = "Task Rejected"
     else flash[:error] = "Task was not Rejected "
-
     end
     redirect_to  taskstab_project_path(@task.project_id)
 
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
+  # Use callbacks to share common setup or constraints between actions.
+  def set_task
+    @task = Task.find(params[:id]) rescue nil
+    if @task.blank?
+      redirect_to '/'
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def task_params
-      params.require(:task).permit(:references, :deadline, :target_number_of_participants, :project_id, :short_description, :number_of_participants, :proof_of_execution, :title, :description, :budget, :user_id, :condition_of_execution, :fileone, :filetwo, :filethree, :filefour, :filefive)
+  end
+  def validate_user
+    unless  current_user.id == @task.project.user_id
+      flash[:error] = "You are Not authorized  to do this operation "
+      redirect_to   taskstab_project_path(@task.project_id)
     end
+  end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def task_params
+    params.require(:task).permit(:references, :deadline, :target_number_of_participants, :project_id, :short_description, :number_of_participants, :proof_of_execution, :title, :description, :budget, :user_id, :condition_of_execution, :fileone, :filetwo, :filethree, :filefour, :filefive)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -17,7 +17,7 @@ class Task < ActiveRecord::Base
 	has_many :donations, dependent: :delete_all
 
 	# after create, assign a Bitcoin address to the task, toggle the comment below to enable
-  after_create :assign_address
+	#after_create :assign_address
 	aasm :column => 'state', :whiny_transitions => false do
     state :pending
 		state :suggested_task
