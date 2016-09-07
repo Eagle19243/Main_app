3e8ea15f8c3cf85b970f97cafcdf12e84fdd3cdd
anton <ant.punko@gmail.com>
2016-09-08 01:22:21 +0300

    Simplified and moved dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,23 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!, :except => :show
 
   def dashboard
-    @do_requests = DoRequest.all
+    @user = current_user
+    @conversations = []
+    #if (current_user && current_user.id == @user.id)
+      @conversations = Conversation.where("recipient_id = ? OR sender_id = ?", params[:id], params[:id])
+      if @conversations.count == 0
+        first_user_with_conversations = User.first
+        recipient = first_user_with_conversations
+        cfirst = Conversation.new(sender_id: current_user.id, recipient_id: recipient.id)
+        if cfirst.save
+          cfirst.messages.create(body: "A test message", user_id: current_user, read: false)
+          @conversations.push(cfirst)
+        end
+      end
+    #end
+    @notifications = Notification.last(5)
     @projects = Project.all
+    @do_requests = DoRequest.all
     @assignments = Assignment.all
-    @proj_admins = ProjAdmin.all
   end
 end
