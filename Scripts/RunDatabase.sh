3bd1b7c001cbc818fd836ad3eec08971d5a62f62
AteqEjaz <ateq.odesk@gmail.com>
2016-09-22 16:08:14 +0500

    RubDatabase.sh updated

--- Scripts/RunDatabase.sh
+++ Scripts/RunDatabase.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+cd /home/ubuntu/appname
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production bundle exec rake db:seed
 RAILS_ENV=production bundle exec rake assets:precompile
