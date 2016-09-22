5511ba18f8e4e3a1c79277662684f6903251de9f
AteqEjaz <ateq.odesk@gmail.com>
2016-09-22 16:13:26 +0500

    remove bundle exec

--- Scripts/RunDatabase.sh
+++ Scripts/RunDatabase.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/appname
-RAILS_ENV=production bundle exec rake db:migrate
-RAILS_ENV=production bundle exec rake db:seed
-RAILS_ENV=production bundle exec rake assets:precompile
+RAILS_ENV=production rake db:migrate
+RAILS_ENV=production rake db:seed
+RAILS_ENV=production rake assets:precompile
