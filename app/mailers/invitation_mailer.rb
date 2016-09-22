8ab2e82734729140fe4e19095a1206fa20403ce9
AteqEjaz <ateq.odesk@gmail.com>
2016-09-22 14:25:24 +0500

    edit invitation mailer

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,6 +1,6 @@
 class InvitationMailer < ApplicationMailer
   def invite_user(email,user_name,task )
-    @task_title=task
+    @task=task
     @user_name =user_name
     mail(to: email, subject: 'invitation For Task')
   end
