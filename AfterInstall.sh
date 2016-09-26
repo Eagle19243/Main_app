1760c5d3937ed40706a21c405c0ee2b9510d6242
BhumishShah <meetbhumish@gmail.com>
2016-09-26 20:30:24 +0530

    Update AfterInstall.sh

--- AfterInstall.sh
+++ AfterInstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/appname/
+bundle install
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production bundle exec rake db:seed
 RAILS_ENV=production bundle exec rake assets:precompile
