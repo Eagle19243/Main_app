1ed027aab8d3b8bb4a07cdb07c7da9e19d024139
anton <ant.punko@gmail.com>
2016-09-28 01:51:56 +0300

    Solved user role inplace editing issue

--- app/controllers/team_memberships_controller.rb
+++ app/controllers/team_memberships_controller.rb
@@ -1,17 +1,20 @@
 class TeamMembershipsController < ApplicationController
-  before_action :set_team, only: [:show, :edit, :update, :destroy]
-
+  
   def update
+    @team_membership = TeamMembership.find(params[:id])
+
+    respond_to do |format|
+      if @team_membership.update_attributes(update_params)
+        format.json { respond_with_bip(@team_membership) }
+      else
+        format.json { respond_with_bip(@user) }
+      end
+    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_team
-      @team = Team.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
-    def team_params
-      params.require(:team).permit(:name, :number_of_members, :number_of_projects, :mission)
+    def update_params
+      params.require(:team_membership).permit(:role)
     end
 end
