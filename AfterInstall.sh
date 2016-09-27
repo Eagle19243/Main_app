51ff2207c02132d579b956602af689dbd514d2b4
P.S.R Patnaik <psrpatnaik@gmail.com>
2016-09-27 17:54:38 +0530

    Update AfterInstall.sh

--- AfterInstall.sh
+++ AfterInstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd /home/ubuntu/appname/
-echo "cd executed" >> /home/ubuntu/log
+echo "after install cd executed " >> /home/ubuntu/log
 RAILS_ENV=production bundle exec rake db:migrate
-echo "rake executed" >> /home/ubuntu/log
+echo "after install rake executed" >> /home/ubuntu/log
 RAILS_ENV=production bundle exec rake db:seed
-echo "rake db:seed executed" >> /home/ubuntu/log
+echo "after install rake db:seed executed" >> /home/ubuntu/log
 RAILS_ENV=production bundle exec rake assets:precompile
-echo "rake precompile executed" >> /home/ubuntu/log
+echo "after install rake precompile executed" >> /home/ubuntu/log
 bundle install
-echo " bundle executed" >> /home/ubuntu/log
+echo " after install bundle executed" >> /home/ubuntu/log
