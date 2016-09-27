100b2f7c3f3a343fd1c32e4b0ced46184cdff9cd
AteqEjaz <ateq.odesk@gmail.com>
2016-09-27 20:01:28 +0500

    place bundle install before rake

--- AfterInstall.sh
+++ AfterInstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/appname/
+bundle install
+echo " after install bundle executed" >> /home/ubuntu/log
 echo "after install cd executed " >> /home/ubuntu/log
 RAILS_ENV=production bundle exec rake db:migrate
 echo "after install rake executed" >> /home/ubuntu/log
@@ -7,6 +9,5 @@ RAILS_ENV=production bundle exec rake db:seed
 echo "after install rake db:seed executed" >> /home/ubuntu/log
 RAILS_ENV=production bundle exec rake assets:precompile
 echo "after install rake precompile executed" >> /home/ubuntu/log
-bundle install
-echo " after install bundle executed" >> /home/ubuntu/log
+
 
