f92f71e5976f7a49c812d9fb02d613d32d8f9f8c
AteqEjaz <ateq.odesk@gmail.com>
2016-10-02 22:23:31 +0500

    Seeds.rb and update tasktab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name:  "Test User",
-             email: "test@test.com",
-             password:              "test1234",
-             password_confirmation: "test1234")
+# User.create!(name:  "Test User",
+#              email: "test@test.com",
+#              password:              "test1234",
+#              password_confirmation: "test1234")
+
+Project.create(title: "Seed Project Ateq",
+               user_id:User.first,
+               state: "pending")
+Task.create(title: "Seed Task",
+            user_id:User.first,
+project_id:Project.where(title: "Seed Project Ateq").first.id,
+            state: "pending")
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 count = CreateAdminService.new.create_additional_admins
