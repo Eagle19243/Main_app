7bd91162c415cd07383269ecda153809a348e377
AteqEjaz <ateq.odesk@gmail.com>
2016-11-09 23:01:35 +0500

    Redirects to same Page and change Error Message

--- app/controllers/user_wallet_transactions_controller.rb
+++ app/controllers/user_wallet_transactions_controller.rb
@@ -26,18 +26,19 @@ class UserWalletTransactionsController < ApplicationController
       if satoshi_amount.eql?('error') or satoshi_amount.blank?
         # satoshi_amount=150761.0
         respond_to do |format|
-          format.html { redirect_to user_path(current_user.id) , alert: 'Error, Please try again Later!' }
+          format.html { redirect_to user_wallet_transactions_new_path , alert: 'Error, Please try again Later!' }
         end
       else
         access_token = access_wallet
         address_from = current_user.user_wallet_address.wallet_id
         sender_wallet_pass_phrase = current_user.user_wallet_address.pass_phrase
         address_to = params['wallet_transaction_user_wallet'].strip
+
         api = Bitgo::V1::Api.new(Bitgo::V1::Api::EXPRESS)
         @res = api.send_coins_to_address(wallet_id: address_from, address: address_to, amount:satoshi_amount , wallet_passphrase: sender_wallet_pass_phrase, access_token: access_token)
         unless @res["message"].blank?
           respond_to do |format|
-            format.html {redirect_to user_path(current_user.id)   , alert: @res["message"] }
+            format.html {redirect_to user_wallet_transactions_new_path  , alert: @res["message"] }
           end
         else
           @transfer.tx_hash = @res["tx"]
@@ -45,19 +46,23 @@ class UserWalletTransactionsController < ApplicationController
 
           if(@transfer.save!)
             respond_to do |format|
-              format.html {redirect_to user_path(current_user.id)  , notice: " #{params["amount"]} usd has been successfully sent to #{@transfer.user_wallet}." }
+              format.html {redirect_to user_wallet_transactions_new_path  , notice: " #{params["amount"]} usd has been successfully sent to #{@transfer.user_wallet}." }
 
             end
           else
             respond_to do |format|
-              format.html { redirect_to user_path(current_user.id)  , alert: @transfer.errors.messages.inspect }
+              format.html { redirect_to user_wallet_transactions_new_path  , alert: @transfer.errors.messages.inspect }
             end
           end
         end
       end
     rescue => e
       respond_to do |format|
-        format.html { redirect_to user_path(current_user.id)   , alert: e.inspect }
+        msg = e.inspect
+        msg.slice! "#<Bitgo::V1::ApiError: "
+        msg = msg.chomp ">"
+
+        format.html { redirect_to user_wallet_transactions_new_path  , alert: msg }
       end
     end
   end
