5a6e0c3fe96d72e5a357f83a260937220bc1f22a
Garon <garon.dev.1@gmail.com>
2016-11-16 21:43:12 +0100

    revert application.yml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
     redirect_to main_app.root_url, notice: exception.message
   end
 
-  # rescue_from ActiveRecord::RecordNotFound do |exception|
-  #   redirect_to main_app.root_url, notice: exception.message
-  # end
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    redirect_to main_app.root_url, notice: exception.message
+  end
 
   protect_from_forgery with: :exception
   around_filter :set_current_user
