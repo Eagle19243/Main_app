f4ebca1607cadbadbdc850e5921c449bc743cc4f
Vedmaka <god.vedmaka@gmail.com>
2016-11-29 13:42:09 +0300

    Test for Travis, changed mailer options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,8 +22,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = false
 
   config.assets.digest = true
   config.assets.raise_runtime_errors = true
