5ecb64172535431691597960c0dfe830d6196a6d
softcomet <davesoft110@gmail.com>
2016-12-01 14:30:56 +0100

    Ignored team relations string

--- app/models/project.rb
+++ app/models/project.rb
@@ -89,7 +89,8 @@ class Project < ActiveRecord::Base
   end
 
   def team_relations_string
-    self.team.team_memberships.count.to_s
+    # self.team.team_memberships.count.to_s
+    # tasks.sum(:number_of_participants).to_s + " / " + tasks.sum(:target_number_of_participants).to_s
   end
 
   def rate_avg
