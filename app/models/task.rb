49b1723fa24874456e7603130c84c6a3d40129c2
koderplus <koderplus@gmail.com>
2017-01-03 23:14:26 +0500

    fix team-member status

--- app/models/task.rb
+++ app/models/task.rb
@@ -178,7 +178,7 @@ class Task < ActiveRecord::Base
   end
 
   def is_executer(user_id)
-    users = self.project.team.team_memberships.where(role: 4 ).collect(&:team_member_id)
+    users = self.project.team.team_memberships.where(role: 3 ).collect(&:team_member_id)
     (users.include? user_id) ? true : false
   end
   def is_team_member( user_id )
