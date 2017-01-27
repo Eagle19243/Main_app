fe3dabbf66c7c2d00633a370ffc9dba98f86ea6c
Garon <garon.dev.1@gmail.com>
2017-01-27 19:13:04 +0100

    Permissions - delete task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -160,7 +160,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       @task_memberships = @task.team_memberships
-      if user_signed_in? && (current_user.is_project_leader?(@task.project) || current_user.is_executor_for?(@task.project) || (@task.suggested_task? && (current_user.id == @task.user_id))) && (current_user.any_fundings? == false)
+      if user_signed_in? && (current_user.is_project_leader?(@task.project) || current_user.is_executor_for?(@task.project) || (@task.suggested_task? && (current_user.id == @task.user_id))) && (@task.any_fundings? == false)
         if @task.update(task_params)
           activity = current_user.create_activity(@task, 'edited')
           format.html { redirect_to @task, notice: 'Task was successfully updated.' }
@@ -181,7 +181,7 @@ class TasksController < ApplicationController
   # DELETE /tasks/1.json
   def destroy
     project= @task.project
-    if (@task.accepted? || @task.pending?)&& (@task.is_leader(current_user.id) || current_user.id == project.user_id)
+    if user_signed_in? && (current_user.is_project_leader?(@task.project) || current_user.is_executor_for?(@task.project) || (@task.suggested_task? && (current_user.id == @task.user_id))) && (@task.any_fundings? == false)
       @task.destroy
       respond_to do |format|
         activity = current_user.create_activity(@task, 'deleted')
