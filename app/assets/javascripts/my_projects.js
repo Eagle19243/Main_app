7bb971e3e2c15f907060cf102f11b75c113bb562
koderplus <koderplus@gmail.com>
2017-01-27 15:52:07 +0500

    Remove leader JS Fix

--- app/assets/javascripts/my_projects.js
+++ app/assets/javascripts/my_projects.js
@@ -1,4 +1,3 @@
 $(document).on("click.updateProjectId", "[data-modal='#change-leader-modal']", function(e) {
-    var projectId = $(e.relatedTarget).data("id");
-    $(this).find("#project_id").val(projectId);
+    $("#project_id").val($(this).data("id"));
 });
