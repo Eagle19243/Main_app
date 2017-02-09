38b2427dc30e13c24dfad2842487f8b9156cd6d3
Sharipov Ruslan <sharipov.ru@gmail.com>
2017-02-09 23:23:44 +0600

    Add specs for descriptions updater service

---
+++ spec/services/project_descriptions_updater_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+require './lib/services/project_descriptions_updater'
+
+RSpec.describe ProjectDescriptionsUpdater, vcr: { cassette_name: 'bitgo' } do
+  let(:owner) { FactoryGirl.create(:user, confirmed_at: Time.now) }
+
+  describe '#sync_project' do
+    let(:project) { FactoryGirl.create(:project, user: owner) }
+
+    it 'updates full_description from the latest revision' do
+      mock_mediawiki_response!(project: project, text: 'foobar')
+
+      described_class.sync_project(project)
+      expect(project.full_description).to eq('foobar')
+    end
+  end
+
+  describe '#sync_all_projects' do
+    let(:count) { 3 }
+    let(:projects) { FactoryGirl.create_list(:project, count, user: owner) }
+
+    it 'invokes sync_project for all existing projects in DB' do
+      projects.each do |project|
+        expect(described_class).to receive(:sync_project).with(project)
+        mock_mediawiki_response!(project: project, text: 'foobar')
+      end
+
+      described_class.sync_all_projects
+    end
+  end
+
+  private
+  def mock_mediawiki_response!(project:, text:)
+    allow(project).to receive(:get_latest_revision).and_return(text)
+  end
+end
