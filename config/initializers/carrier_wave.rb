0b29cc414b9ce000f1614011ad5854fb3d105e22
Sharipov Ruslan <sharipov.ru@gmail.com>
2017-03-03 15:02:51 +0600

    Do not call config.fog_credentials for development and test environments (#685)

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,24 +2,16 @@ CarrierWave.configure do |config|
 
   # Use local storage if in development or test
   if Rails.env.development? || Rails.env.test?
-    CarrierWave.configure do |config|
-      config.storage = :file
-    end
+    config.storage = :file
+  else
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => ENV["service_provider"],
+      :aws_access_key_id      => ENV["aws_access_key_id"],
+      :aws_secret_access_key  => ENV["aws_secret_access_key"],
+      :region                 => ENV["s3_bucket_region"]
+    }
+
+    config.fog_directory  = ENV["bucket_name"]
   end
-
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.storage = :fog
-    end
-  end
-
-  config.fog_credentials = {
-    :provider               => ENV["service_provider"],
-    :aws_access_key_id      => ENV["aws_access_key_id"],
-    :aws_secret_access_key  => ENV["aws_secret_access_key"],
-    :region                 => ENV["s3_bucket_region"]
-  }
-
-  config.fog_directory  = ENV["bucket_name"]
-
 end
