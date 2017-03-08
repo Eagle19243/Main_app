c4c90806bd96e83bfffe26c930da6a72028d20e5
Vibol <tvcam@users.noreply.github.com>
2017-03-08 21:48:40 +0700

    fix notification on following project (WS-128) (#704)
    * fix notification on following project
    
    * add spec

---
+++ spec/features/notification_after_follow_project_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'Notification After Follow the Project', js: true, vcr: { cassette_name: 'bitgo' } do
+  let(:user)          { create(:user, :confirmed_user) }
+  let(:project)       { create(:project) }
+
+  before do
+    login_as(user, scope: :user, run_callbacks: false)
+    visit project_path(project)
+  end
+
+  context 'notifications on top menu' do
+    scenario 'notifies user on follow a project' do
+      find('.btn-root.follow-unfollow').click
+      wait_for_ajax
+
+      expect(page).to have_content('You are now following project')
+    end
+  end
+end
