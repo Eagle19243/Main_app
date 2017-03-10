714a9ae15d1e6ab39c009f0af53e5a3ad0e2338f
Eagle19243 <eagle19243@gmail.com>
2017-03-10 12:46:22 +0100

    Extract task wallet creation logic into the service

--- app/services/payments/btc/create_user_wallet_service.rb
+++ app/services/payments/btc/create_user_wallet_service.rb
@@ -14,11 +14,9 @@ class Payments::BTC::CreateUserWalletService
     secure_passphrase =  user.encrypted_password
     secure_label = SecureRandom.hex(5)
 
-    wallet_handler = Payments::BTC::WalletHandler.new
-    new_wallet_id, userKeychain, backupKeychain  = wallet_handler.create_wallet(secure_passphrase, secure_label)
-
-    new_wallet_address_sender =  wallet_handler.create_address(new_wallet_id, "1")
-    new_wallet_address_receiver = wallet_handler.create_address(new_wallet_id, "0")
+    wallet_creation_service = Payments::BTC::CreateWalletService.new(secure_passphrase, secure_label)
+    new_wallet_id, new_wallet_address_sender, new_wallet_address_receiver, userKeychain, backupKeychain = wallet_creation_service.call
+    
     UserWalletAddress.create(sender_address: new_wallet_address_sender, receiver_address: new_wallet_address_receiver, pass_phrase: secure_passphrase, user_id: user.id, wallet_id: new_wallet_id, user_keys: userKeychain, backup_keys: backupKeychain)
   end
 end
