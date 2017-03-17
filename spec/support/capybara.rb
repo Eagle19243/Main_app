73cc7fdc722381e3ca66ca34c729b00b8ba59979
Sharipov Ruslan <sharipov.ru@gmail.com>
2017-03-18 02:43:20 +0600

    set timeout to 15 (old value was 120!) (#762)

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -7,7 +7,7 @@ Capybara.register_driver :poltergeist do |app|
   options = {
       :window_size => [1600, 900],
       :screen_size => [1600, 900],
-      :timeout => 120
+      :timeout => 15
   }
 
   Capybara::Poltergeist::Driver.new(app, options)
@@ -16,7 +16,7 @@ end
 Capybara.javascript_driver = :poltergeist
 Capybara.ignore_hidden_elements = false
 Capybara.server_port = 35792
-Capybara.default_max_wait_time = 120
+Capybara.default_max_wait_time = 15
 
 RSpec.configure do |c|
   c.include Capybara::DSL
