e0442cc09efd7e486a3d8f7de5f21518c7f7e53e
Eagle19243 <eagle19243@gmail.com>
2017-03-20 15:47:25 +0100

    Revert "set timeout to 15 (old value was 120!) (#762)" (#770)
    This reverts commit 912f8acf26d6c864a35a4b6278229b615028179b.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -7,7 +7,7 @@ Capybara.register_driver :poltergeist do |app|
   options = {
       :window_size => [1600, 900],
       :screen_size => [1600, 900],
-      :timeout => 15
+      :timeout => 120
   }
 
   Capybara::Poltergeist::Driver.new(app, options)
@@ -16,7 +16,7 @@ end
 Capybara.javascript_driver = :poltergeist
 Capybara.ignore_hidden_elements = false
 Capybara.server_port = 35792
-Capybara.default_max_wait_time = 15
+Capybara.default_max_wait_time = 120
 
 RSpec.configure do |c|
   c.include Capybara::DSL
