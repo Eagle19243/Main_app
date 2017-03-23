33b0ffdfa4d89d62cdf68ea8c2455783b10359fe
Sharipov Ruslan <sharipov.ru@gmail.com>
2017-03-23 18:05:48 +0600

    Disable transfers logic from task model (#786)

--- app/models/task.rb
+++ app/models/task.rb
@@ -63,45 +63,12 @@ class Task < ActiveRecord::Base
     tasks.limit(limit)
   end
 
-  def transfer_to_user_wallet(wallet_address_to_send_btc, amount)
-    transfering_task = self
-    @transfer = WalletTransaction.new(amount: amount, user_wallet: wallet_address_to_send_btc, task_id: self.id)
-    satoshi_amount = amount if @transfer.valid?
-    if (satoshi_amount.eql?('error') or satoshi_amount.blank?)
-      @transfer.save
-    else
-      access_token = Payments::BTC::Base.bitgo_access_token
-      address_from = transfering_task.wallet_address.wallet_id
-      sender_wallet_pass_phrase = transfering_task.wallet_address.pass_phrase
-      address_to = wallet_address_to_send_btc.strip
-      @res = api.send_coins_to_address(wallet_id: address_from, address: address_to, amount: satoshi_amount, wallet_passphrase: sender_wallet_pass_phrase, access_token: access_token)
-      unless @res["message"].blank?
-        @transfer.save
-      else
-        @transfer.tx_hex = @res["tx"]
-        @transfer.tx_id = @res["hash"]
-        @transfer.task_id = transfering_task.id
-        @transfer.save
-      end
-    end
-  end
-
-  def stripe_refund (funded_by_stripe, bitgo_fee)
-    funded_by_stripe.each do |stripe_payment|
-      total_bitgo_fee = ((stripe_payment.amount_in_satoshi * bitgo_fee) / 100)
-      transfer_to_user = stripe_payment.amount_in_satoshi - total_bitgo_fee.to_i
-      user_wallet = UserWalletAddress.where(user_id: stripe_payment.user_id).first
-      transfer_to_user_wallet(user_wallet.sender_address,transfer_to_user)
-    end
+  def stripe_refund(funded_by_stripe, bitgo_fee)
+    raise NotImplementedError, "Refunds are disabled now"
   end
 
   def user_wallet_refund(funds_from_user_wallets, bitgo_fee)
-    funds_from_user_wallets.each do |wallet_transaction |
-      total_bitgo_fee = (wallet_transaction.amount * bitgo_fee) / 100
-      transfer_to_user = wallet_transaction.amount - total_bitgo_fee.to_i
-      user_wallet = UserWalletAddress.where(user_id:  wallet_transaction.user_id).first
-      transfer_to_user_wallet(user_wallet.sender_address,transfer_to_user)
-    end
+    raise NotImplementedError, "Refunds are disabled now"
   end
 
   def not_fully_funded_or_less_teammembers?
@@ -170,10 +137,6 @@ class Task < ActiveRecord::Base
   end
 
   private
-  def api
-    Bitgo::V1::Api.new
-  end
-
   def amount_after_bitgo_fee(amount)
     (amount - (amount * Payments::BTC::Base.bitgo_fee))
   end
