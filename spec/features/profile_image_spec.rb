7ebc3dfc55ffb896ebdd687b1126b56ddf1a5e5b
Alex Shcherba <alex@front-devs.com>
2017-04-12 11:31:07 +0300

    Prevent hide edit images profile buttons (WS-172) (#839)
    * Prevent hide edit images profile buttons
    
    * fix spec
    
    * Fixed specs by adding forgotten variable

--- spec/features/profile_image_spec.rb
+++ spec/features/profile_image_spec.rb
@@ -18,68 +18,47 @@ feature "Profile Image", js: true do
         @edit_link = find(".btn-edit-image._profile")
       end
 
-      scenario "Then 'Edit My Profile Image' link not appeared" do
-        expect(@edit_link).not_to be_visible
+      scenario "Then 'Edit My Profile Image' link appeared" do
+        expect(@edit_link).to be_visible
       end
 
-      context "When you hover mouse on the profile image" do
+      context "When you click 'Edit My Profile Image' link" do
         before do
-          @image_div = find(".profile-hero__user-portrait._signed-in")
-          @image_div.hover          
+          @edit_link.click
+          wait_for_ajax
+
+          @modal = find(".modal-edit-img")
         end
 
-        scenario "Then 'Edit My Profile Image' link appeared" do
-          expect(@edit_link).to be_visible
+        scenario "Then 'Edit Profile Image' modal appeared" do
+          expect(@modal).to be_visible
         end
 
-        context "When you click 'Edit My Profile Image' link" do
+        context "When you choose another image and click 'SAVE CHANGES' button" do
           before do
-            @edit_link.click
+            @image = "user_photo.png"
+            @modal.attach_file 'user[picture]', Rails.root + "spec/fixtures/#{@image}"
             wait_for_ajax
 
-            @modal = find(".modal-edit-img")
+            @modal.click_button "Save Changes"
           end
 
-          scenario "Then 'Edit Profile Image' modal appeared" do
-            expect(@modal).to be_visible
-          end
-
-          context "When you choose another image and click 'SAVE CHANGES' button" do
-            before do
-              @image = "user_photo.png"
-              @modal.attach_file 'user[picture]', Rails.root + "spec/fixtures/#{@image}"
-              wait_for_ajax
-
-              @modal.click_button "Save Changes"
-            end
-
-            scenario "Then your profile image has been changed" do
-              expect(@image_div).to have_xpath("//img[contains(@src, @image)]")
-            end
+          scenario "Then your profile image has been changed" do
+            @image_div = find(".profile-hero__user-portrait._signed-in")
+            expect(@image_div).to have_xpath("//img[contains(@src, @image)]")
           end
         end
       end
     end
+  end
 
-    context "When you visit other's profile page" do
-      before do
-        visit user_path(@another_user)
-      end
+  context "When you visit other's profile page" do
+    before do
+      visit user_path(@another_user)
+    end
 
-      scenario "Then 'Edit My Profile Image' link not appeared" do
-        expect(page).not_to have_selector(".btn-edit-image._profile")
-      end
-
-      context "When you hover mouse on the profile image" do
-        before do
-          @image_div = find(".profile-hero__user-portrait")
-          @image_div.hover          
-        end
-
-        scenario "Then 'Edit My Profile Image' link not appeared" do
-          expect(page).not_to have_selector(".btn-edit-image._profile")
-        end
-      end
+    scenario "Then 'Edit My Profile Image' link not appeared" do
+      expect(page).not_to have_selector(".btn-edit-image._profile")
     end
   end
 
@@ -91,17 +70,6 @@ feature "Profile Image", js: true do
 
       scenario "Then 'Edit My Profile Image' link not appeared" do
         expect(page).not_to have_selector(".btn-edit-image._profile")
-      end
-
-      context "When you hover mouse on the profile image" do
-        before do
-          @image_div = find(".profile-hero__user-portrait")
-          @image_div.hover          
-        end
-
-        scenario "Then 'Edit My Profile Image' link not appeared" do
-          expect(page).not_to have_selector(".btn-edit-image._profile")
-        end
       end
     end
   end
