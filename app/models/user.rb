a745d1f930ab24029a0347b11ae190fb0bc18836
Eagle19243 <eagle19243@gmail.com>
2017-04-18 18:11:23 +0200

    shorter password confirmation message

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,7 +57,7 @@ class User < ActiveRecord::Base
   validates_format_of     :email, with: Devise.email_regexp, allow_blank: true, if: :email_changed?
 
   validates_presence_of     :password, if: :password_required?
-  validates_confirmation_of :password, if: :password_required?
+  validates_confirmation_of :password,  message: "doesn't match", if: :password_required?
 
   # Override
   validates_length_of       :password, within: Devise.password_length, allow_blank: true, if: ->(user) { user.errors[:password_confirmation].blank? }
