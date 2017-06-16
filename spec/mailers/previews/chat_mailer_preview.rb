5d6b0db8ee8b91ef26c77561f63d93e19c62870f
Alter Lagos <alter.strife@gmail.com>
2017-06-16 19:06:31 -0400

    Preview mailer

--- spec/mailers/previews/chat_mailer_preview.rb
+++ spec/mailers/previews/chat_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/chat_mailer
 class ChatMailerPreview < ActionMailer::Preview
-
+  def invite_receiver
+    ChatMailer.invite_receiver(User.first.id, User.last.id)
+  end
 end
