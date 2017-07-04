f956e46176b2a7a18048a4a6620c192147701580
Eagle19243 <eagle19243@gmail.com>
2017-07-04 16:26:00 +0200

    remove hardcoded tokens (#1145)
    * remove hardcoded tokens
    
    * Full commit

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  publishable_key: %w(test development staging).include?(Rails.env) ? 'pk_test_ZeUjuyRgxBFMrn0rAVu7156Q' : ENV['PUBLISHABLE_KEY_STRIPE'],
-  secret_key: %w(test development staging).include?(Rails.env) ? 'sk_test_y2wKfJ1NEw2C9yKIouxuI8jo' : ENV['SECRET_KEY_STRIPE']
+  publishable_key: ENV['PUBLISHABLE_KEY_STRIPE'],
+  secret_key: ENV['SECRET_KEY_STRIPE']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
