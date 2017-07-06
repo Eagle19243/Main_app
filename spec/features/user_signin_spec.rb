3c0a2d57c6649e64734ce8d0f71db542a3d97289
vibol <vibolteav@gmail.com>
2017-07-06 08:51:57 +0700

    Remove duplicated JS renderring & fix specs

--- spec/features/user_signin_spec.rb
+++ spec/features/user_signin_spec.rb
@@ -5,14 +5,11 @@ feature 'Sing_in working on  "/users/sign_in" ' , js: true do
 
   scenario 'user logs in at "/users/sign_in" 'do
     visit '/users/sign_in'
-    wrapper = find("#projects-search-results")
-    sign_in_form = wrapper.find('#new_user')
+    sign_in_form = find('.main-form #new_user')
 
     sign_in_form.fill_in 'user_email', with: user.email
-    sign_in_form.fill_in 'user_password', with: user.password
+    sign_in_form.fill_in 'user_password', with: 'secretadmin0password'
     sign_in_form.click_button 'Sign in'
-
-    sleep 1
 
     expect(page).to have_no_content 'Invalid Email or password.'
     expect(page).to have_current_path('/home', only_path: true)
